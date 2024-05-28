28a5d70225d9b037f95254fd83233d30b6040985
Jorin Eggers <jorineggers@gmail.com>
2024-05-28 15:57:11 +0200

    change pad token

--- finetune.py
+++ finetune.py
@@ -36,11 +36,12 @@ model = AutoModelForSequenceClassification.from_pretrained(
     num_labels=3,
 )
 tokenizer = AutoTokenizer.from_pretrained(model_str)
-tokenizer.add_special_tokens(
-    {
-        "pad_token": "<pad>",
-    }
-)  # add pad token to tokenizer for padding
+# tokenizer.add_special_tokens(
+#     {
+#         "pad_token": "<pad>",
+#     }
+# )  # add pad token to tokenizer for padding
+tokenizer.pad_token = tokenizer.eos_token  # set pad token to eos token for padding
 dataset = load_dataset("lmsys/lmsys-arena-human-preference-55k")
 
 max_length = 1000
