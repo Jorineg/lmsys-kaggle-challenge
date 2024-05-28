5c51366bb6e77a475414b2e94e6c70d918c7797e
Jorin Eggers <jorineggers@gmail.com>
2024-05-28 10:55:12 +0200

    set batch size

--- finetune.py
+++ finetune.py
@@ -134,8 +134,8 @@ dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels
 training_args = TrainingArguments(
     output_dir="./results",
     num_train_epochs=1,
-    # per_device_train_batch_size=8,
-    # per_device_eval_batch_size=8,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
     warmup_steps=500,
     weight_decay=0.01,
     logging_dir="./logs",
